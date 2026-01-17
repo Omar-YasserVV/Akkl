e1bc4f1348586ade35ac4041ef87e06b14d952f5
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2026-01-17 07:20:58 +0200

    feat: add Prisma service with PostgreSQL adapter

---
+++ apps/api/libs/shared/src/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const dbUrl = process.env.DATABASE_URL;
+
+    if (!dbUrl) {
+      throw new Error(
+        'DATABASE_URL is not defined in the environment variables',
+      );
+    }
+
+    const pool = new Pool({ connectionString: dbUrl });
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
