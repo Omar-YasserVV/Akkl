8afc19612e0c73e06db7cf8812135f2de0905cde
Fezex <Eyad__Ahmed@outlook.com>
2026-01-18 05:39:28 +0200

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- apps/api/apps/svc-auth/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { SvcAuthModule } from './svc-auth.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(SvcAuthModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
