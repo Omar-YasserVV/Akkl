60acffa91f29accfee0bce5c219d11f0dd49c390
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2026-01-27 04:58:22 +0200

    Add Kafka microservice client configuration to AppModule

--- apps/api/apps/api/src/app.module.ts
+++ apps/api/apps/api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9001'],
+          },
+          consumer: {
+            groupId: 'svc-auth-consumer', // Needed even for producers to handle responses
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
