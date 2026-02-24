097249ff8527437e511d3b331d1badbf4a5fef52
Omar-YasserVV <bakryasser744@gmail.com>
2026-02-24 15:44:08 +0200

    edit logo and title

--- apps/desktop/dist-electron/main.js
+++ apps/desktop/dist-electron/main.js
@@ -12,7 +12,8 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win;
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    icon: path.join(process.env.VITE_PUBLIC, "Logo.ico"),
+    title: "Akkl",
     webPreferences: {
       preload: path.join(__dirname$1, "preload.mjs")
     }
--- apps/desktop/electron/main/main.ts
+++ apps/desktop/electron/main/main.ts
@@ -21,7 +21,8 @@ let win: BrowserWindow | null;
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    icon: path.join(process.env.VITE_PUBLIC, "Logo.ico"),
+    title: "Akkl",
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs"),
     },
