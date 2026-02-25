f6601a45972be54a6bef9f1892bda822eab285c4
Fezex <Eyad__Ahmed@outlook.com>
2026-02-25 17:07:45 +0200

    feat: add AppBreadcrumb component to enhance navigation in the App layout

--- apps/desktop/src/app/App.tsx
+++ apps/desktop/src/app/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { AppBreadcrumb } from "./components/ui/AppBreadcrumb";
 
 function App() {
   return (
     <main>
+      <AppBreadcrumb />
       <Outlet />
     </main>
   );
