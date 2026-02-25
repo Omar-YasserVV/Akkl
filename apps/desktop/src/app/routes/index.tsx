ae5aa5d5703f946e1d1a95f77c1f08e79b22039d
Fezex <Eyad__Ahmed@outlook.com>
2026-02-25 17:01:58 +0200

    fix: update import path for SignIn component in routing

--- apps/desktop/src/app/routes/index.tsx
+++ apps/desktop/src/app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import SignIn from "../pages/auth/SignIn";
+import SignIn from "../pages/auth/sign-in/SignIn";
 import Home from "../pages/home/Home";
 
 export const router = createBrowserRouter([
