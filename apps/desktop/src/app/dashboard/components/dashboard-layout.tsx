34832971e09653bc90f660fdd2e0ca0c13bc17b0
Omar-YasserVV <bakryasser744@gmail.com>
2026-02-26 07:36:11 +0200

    add beadcumb to dashboard

--- apps/desktop/src/app/dashboard/components/dashboard-layout.tsx
+++ apps/desktop/src/app/dashboard/components/dashboard-layout.tsx
@@ -1,18 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./sidebar";
 import Topbar from "./topbar";
+import { AppBreadcrumb } from "@/app/components/ui/AppBreadcrumb";
 
 const DashboardLayout = () => {
   return (
-    <div className="flex h-screen w-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Topbar />
-        <div className="flex-1 overflow-auto">
-          <Outlet />
+    <>
+      <AppBreadcrumb />
+
+      <div className="flex h-screen w-screen">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Topbar />
+          <div className="flex-1 overflow-auto">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
