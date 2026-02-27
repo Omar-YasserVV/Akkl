7b10aa6ff6ff194f1fc074449e406c8fc2e5393d
Fezex <Eyad__Ahmed@outlook.com>
2026-02-27 09:18:03 +0200

    feat: initialize desktop application with core routing and a dashboard overview page displaying recent orders.

--- apps/desktop/src/app/dashboard/pages/overview/index.tsx
+++ apps/desktop/src/app/dashboard/pages/overview/index.tsx
@@ -1,9 +1,13 @@
 import OverviewHeader from "./components/overview-header";
+import RecentOrdersTable from "./components/recent-orders-table";
 
 const Overview = () => {
   return (
     <>
       <OverviewHeader />
+      <div className="grid grid-cols-4 gap-5">
+        <RecentOrdersTable />
+      </div>
     </>
   );
 };
