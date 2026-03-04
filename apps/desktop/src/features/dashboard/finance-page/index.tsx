58ac743478698c5725fb9d89341102166fc6f0cc
Fezex <Eyad__Ahmed@outlook.com>
2026-03-04 20:12:33 +0200

    feat: implement dashboard finance page with charts and expense listings

--- apps/desktop/src/features/dashboard/finance-page/index.tsx
+++ apps/desktop/src/features/dashboard/finance-page/index.tsx
@@ -1,23 +1,21 @@
-import FinanceChartManager from "./components/FinanceChartManager"
-import FinanceHeader from "./components/FinanceHeader"
-import DonutChart from "./components/DonutChart"
-import TopSelling from "./components/TopSelling"
-import RecentExpenses from "./components/RecentExpenses"
+import FinanceChartManager from "./components/FinanceChartManager";
+import FinanceHeader from "./components/FinanceHeader";
+import DonutChart from "./components/DonutChart";
+import TopSelling from "./components/TopSelling";
+import RecentExpenses from "./components/RecentExpenses";
 
 const FinancePage = () => {
-    return (
-        <div className="flex flex-col gap-6">
-            <FinanceHeader />
-            <FinanceChartManager />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-[400px]">
-                <TopSelling />
-                <DonutChart />
-            </div>
-            <RecentExpenses />
-        </div>
-    )
-}
+  return (
+    <>
+      <FinanceHeader />
+      <FinanceChartManager />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-[400px]">
+        <TopSelling />
+        <DonutChart />
+      </div>
+      <RecentExpenses />
+    </>
+  );
+};
 
-
-
-export default FinancePage
+export default FinancePage;
