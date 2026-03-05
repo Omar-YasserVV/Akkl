70a9f8cd0e2d17a50f8b12deaa647ce311a77fa2
Omar-YasserVV <bakryasser744@gmail.com>
2026-03-05 16:48:52 +0200

    fix menu manager header style

--- apps/desktop/src/features/dashboard/menu-manager/components/AddModal/AddModalHeader.tsx
+++ apps/desktop/src/features/dashboard/menu-manager/components/AddModal/AddModalHeader.tsx
@@ -4,7 +4,7 @@ import { IoClose } from "react-icons/io5";
 
 function AddModalHeader({ onClose }: { onClose: () => void }) {
   return (
-    <ModalHeader className=" justify-between items-center">
+    <ModalHeader className=" justify-between items-center border-[#E2E8F0]">
       <div className="flex gap-2 items-center text-default-700">
         <div className="bg-primary/10 p-2.5 rounded-lg">
           <FaUtensils className="w-5 h-5 text-primary" />
@@ -12,7 +12,7 @@ function AddModalHeader({ onClose }: { onClose: () => void }) {
         <span className="font-bold text-xl">Add New Menu Item</span>
       </div>
       <Button isIconOnly variant="light" onPress={onClose}>
-        <IoClose size={30} />
+        <IoClose size={30} className="text-[#64748B]" />
       </Button>
     </ModalHeader>
   );
