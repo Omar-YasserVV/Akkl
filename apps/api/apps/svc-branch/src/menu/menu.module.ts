54744c8218049f9743b424b6dad95e9df11ca65f
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2026-04-01 12:25:39 +0200

    Resolve merge conflict in menu.module.ts

--- apps/api/apps/svc-branch/src/menu/menu.module.ts
+++ apps/api/apps/svc-branch/src/menu/menu.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from '@app/db';
 import { PrismaService } from '@app/db';
-<<<<<<< HEAD
 import { MenuService } from './menu.service';
 
 @Module({
@@ -16,5 +15,3 @@ import { MenuService } from './menu.service';
   providers: [MenuService, PrismaService],
 })
 export class MenuModule {}
-=======
->>>>>>> e2dbb2951c2a80fa93e0a595787261c4499a231e
