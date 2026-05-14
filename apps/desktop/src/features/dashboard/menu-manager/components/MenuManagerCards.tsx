26284194ae42d114d45c32ea3bded4fd4279bfb8
Fezex <Eyad__Ahmed@outlook.com>
2026-05-14 10:57:47 +0300

    feat: improve MenuManagerCards component to display loading skeletons while fetching menu stats

--- apps/desktop/src/features/dashboard/menu-manager/components/MenuManagerCards.tsx
+++ apps/desktop/src/features/dashboard/menu-manager/components/MenuManagerCards.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@heroui/react";
 import { MenuManagerCards_Metadata } from "../constants/MenuManagerCardsConst";
 import { useMenuStats } from "../hooks/useMenuItem";
 import MenuManagerCard from "./MenuManagerCard";
@@ -5,23 +6,24 @@ import MenuManagerCard from "./MenuManagerCard";
 function MenuManagerCards() {
   const { data: menuStats, isLoading } = useMenuStats();
 
-  if (isLoading || !menuStats) return <div>Loading...</div>;
-
   return (
     <div className="grid grid-cols-4 gap-4">
-      {MenuManagerCards_Metadata.map((card) => {
-        // TypeScript now understands that menuStats[card.id] is safe
-        const displayValue = menuStats[card.id];
-
-        return (
-          <MenuManagerCard
-            key={card.id}
-            title={card.title}
-            value={displayValue}
-          />
-        );
-      })}
+      {isLoading || !menuStats
+        ? MenuManagerCards_Metadata.map((card) => (
+            <Skeleton key={card.id} className="h-24 w-full rounded-xl" />
+          ))
+        : MenuManagerCards_Metadata.map((card) => {
+            const displayValue = menuStats[card.id];
+            return (
+              <MenuManagerCard
+                key={card.id}
+                title={card.title}
+                value={displayValue}
+              />
+            );
+          })}
     </div>
   );
 }
+
 export default MenuManagerCards;
