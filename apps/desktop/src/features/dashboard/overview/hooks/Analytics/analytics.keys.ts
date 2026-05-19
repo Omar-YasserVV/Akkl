4e33b45186747ff5a2646dac0c776222bbddbcc6
Omar-YasserVV <bakryasser744@gmail.com>
2026-05-19 19:45:15 +0300

    feat(analytics): implement top selling items API and integrate into TopItems component

--- apps/desktop/src/features/dashboard/overview/hooks/Analytics/analytics.keys.ts
+++ apps/desktop/src/features/dashboard/overview/hooks/Analytics/analytics.keys.ts
@@ -1,13 +1,23 @@
-import { AnalyticsDaysAgo } from "@/features/dashboard/overview/types/analytics";
+import {
+  AnalyticsDaysAgo,
+  GetTopSellingItemsParams,
+} from "@/features/dashboard/overview/types/analytics";
 
 export const analyticsKeys = {
   all: ["analytics"] as const,
 
+  // Revenue Keys
   revenues: () => [...analyticsKeys.all, "revenue"] as const,
   revenue: (daysAgo: AnalyticsDaysAgo) =>
     [...analyticsKeys.revenues(), daysAgo] as const,
 
+  // Orders Keys
   branchOrders: () => [...analyticsKeys.all, "orders"] as const,
   branchOrder: (daysAgo: AnalyticsDaysAgo) =>
     [...analyticsKeys.branchOrders(), daysAgo] as const,
+
+  // Top Selling Items Keys
+  topSellings: () => [...analyticsKeys.all, "top-selling"] as const,
+  topSelling: (params: GetTopSellingItemsParams) =>
+    [...analyticsKeys.topSellings(), params] as const,
 };
--- apps/desktop/src/features/dashboard/overview/hooks/Analytics/useAnalytics.ts
+++ apps/desktop/src/features/dashboard/overview/hooks/Analytics/useAnalytics.ts
@@ -1,27 +1,27 @@
-import { AnalyticsDaysAgo } from "@/features/dashboard/overview/types/analytics";
-import { LineChartAnalyticsResponseDto } from "@/types/analytics";
-import { apiClient } from "@repo/utils";
+import {
+  AnalyticsDaysAgo,
+  GetTopSellingItemsParams,
+} from "@/features/dashboard/overview/types/analytics";
 import { useQuery } from "@tanstack/react-query";
+import { analyticsApis } from "../../api/analytics";
 import { analyticsKeys } from "./analytics.keys";
-
-const BASE_URL = "/analytics";
 
 export const useBranchRevenueQuery = (daysAgo: AnalyticsDaysAgo) =>
   useQuery({
     queryKey: analyticsKeys.revenue(daysAgo),
-    queryFn: () =>
-      apiClient.get<LineChartAnalyticsResponseDto>(
-        `${BASE_URL}/branch/revenue-analytics`,
-        { params: { daysAgo } },
-      ),
+    queryFn: () => analyticsApis.getBranchRevenue(daysAgo),
   });
 
 export const useBranchOrdersQuery = (daysAgo: AnalyticsDaysAgo) =>
   useQuery({
     queryKey: analyticsKeys.branchOrder(daysAgo),
-    queryFn: () =>
-      apiClient.get<LineChartAnalyticsResponseDto>(
-        `${BASE_URL}/branch/orders-analytics`,
-        { params: { daysAgo } },
-      ),
+    queryFn: () => analyticsApis.getBranchOrders(daysAgo),
+  });
+
+export const useTopSellingItemsQuery = (
+  params: GetTopSellingItemsParams = {},
+) =>
+  useQuery({
+    queryKey: analyticsKeys.topSelling(params),
+    queryFn: () => analyticsApis.getTopSellingItems(params),
   });
